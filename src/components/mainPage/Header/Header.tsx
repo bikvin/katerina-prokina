1728fd530db49df9d4fdbd04ad34718e55d71f63
Ivan Kraev <kukurua.tuni@gmail.com>
2025-10-22 19:05:40 +0400

    remove_items_from_top_menu

--- src/components/mainPage/Header/Header.tsx
+++ src/components/mainPage/Header/Header.tsx
@@ -44,10 +44,10 @@ export default function Header({ menuHeader }: { menuHeader: string }) {
   const menuItems = [
     { href: "/#about", label: "Обо мне" },
     { href: "/#when-needed", label: "Когда необходимо" },
-    { href: "/#results", label: "Результаты" },
+    // { href: "/#results", label: "Результаты" },
     { href: "/#forclients", label: "Клиентам" },
     { href: "/articles", label: "Cтатьи" },
-    { href: "/#movieclub", label: "Киноклуб" },
+    // { href: "/#movieclub", label: "Киноклуб" },
     { href: "/#contacts", label: "Контакты" },
   ];
 
